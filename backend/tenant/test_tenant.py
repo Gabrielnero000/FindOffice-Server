c2b810038d20e10c08081b5bcdf140f7234db542
Jorge Gomes <httpmelo@gmail.com>
2020-11-14 18:02:21 -0300

    feature delete_office added

--- backend/tenant/test_tenant.py
+++ backend/tenant/test_tenant.py
@@ -7,6 +7,11 @@ def testGetOffices():
     tenant_id = 0
     print(tenant_api.getOffices(tenant_id))
 
+def testExclude_office():
+    tenant_api = TenantApi()
+
+    office_id = 0
+    print(tenant_api.exclude_office(office_id))
 
 if __name__ == "__main__":
     fire.Fire()
