150bf5a0efc23ce6c0aa2ccc65ed3d86dbbd07a5
florencerc <74015114+florencerc@users.noreply.github.com>
2020-11-30 13:11:12 -0300

    Update tables.py

--- backend/db/tables.py
+++ backend/db/tables.py
@@ -6,28 +6,38 @@ TABLES['users'] = (
     "  name NVARCHAR(180) NOT NULL,"
     "  email NVARCHAR(255) NOT NULL UNIQUE,"
     "  password NVARCHAR(180) NOT NULL,"
-    "  legalPerson BIT NOT NULL,"
-    "  cpf NCHAR(11),"
-    "  cnpj NCHAR(14),"
-    "  isTenant BIT NOT NULL,"
+    "  type NVARCHAR(180) NOT NULL,"
+    "  PRIMARY KEY (userId))")
+
+TABLES['tenant'] = (
+    "CREATE TABLE users ("
+    "  userId INT NOT NULL,"
+    "  PRIMARY KEY (userId))")
+
+TABLES['landlord'] = (
+    "CREATE TABLE users ("
+    "  userId INT NOT NULL,"
     "  PRIMARY KEY (userId))")
 
 TABLES['offices'] = (
     "CREATE TABLE offices ("
     "  officeId INT NOT NULL AUTO_INCREMENT,"
-    "  ownerId INT NOT NULL,"
+    "  landlordId INT NOT NULL,"
     "  address NVARCHAR(300) NOT NULL,"
     "  district NVARCHAR(300) NOT NULL,"
     "  number NVARCHAR(5) NOT NULL,"
-    "  extra NVARCHAR(300) NOT NULL,"
+    "  description NVARCHAR(300) NOT NULL,"
     "  scoring INT NOT NULL,"
     "  nScore INT NOT NULL,"
+    "  daily_rate FLOAT NOT NULL,"
+    "  type VARCHAR(100) NOT NULL,"
     "  PRIMARY KEY (officeId))")
 
 TABLES['rents'] = (
     "CREATE TABLE rents ("
     "  rentId INT NOT NULL AUTO_INCREMENT,"
-    "  userId INT NOT NULL,"
+    "  officeId INT NOT NULL,"
+    "  tenantId INT NOT NULL,"
     "  bookingStart DATETIME NOT NULL,"
     "  bookingEnd DATETIME NOT NULL,"
     "  checkIn DATETIME,"
