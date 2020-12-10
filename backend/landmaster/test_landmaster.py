d015e75c774fe413cccfb5a91fb3d429597dea56
florencerc <74015114+florencerc@users.noreply.github.com>
2020-12-09 22:49:55 -0300

    resolvendo conflitos

--- backend/landmaster/test_landmaster.py
+++ backend/landmaster/test_landmaster.py
@@ -71,26 +71,18 @@ def testGetTotalValue():
     id_landmaster = 0
     print(landmaster_api.getTotalValue(id_landmaster))
 
-<<<<<<< HEAD
-def testGet_top_value_office():
-    landmaster_api = LandmasterApi()
-
-    id_landmaster = 1
-    print(landmaster_api.get_top_value_office(id_landmaster))
-=======
-def testGetTotalValue():
-    landmaster_api = LandmasterApi()
-
-    id_landmaster = 0
-    print(landmaster_api.getTotalValue(id_landmaster))
-
 def testTopRentsOffice():
     landmaster_api = LandmasterApi()
 
     id_landmaster = 1
     print(landmaster_api.topRentsOffice(id_landmaster))
 
->>>>>>> main
+def testGet_top_value_office():
+    landmaster_api = LandmasterApi()
+
+    id_landmaster = 1
+    print(landmaster_api.get_top_value_office(id_landmaster))
+
 
 if __name__ == "__main__":
     fire.Fire()
